d26db24ac3808d2e69b0e2a14e16ee54a6d5a43f
Umam <umam.tekno@gmail.com>
2019-09-07 20:02:18 +0700

    add latarbelakang

--- application/views/informasi/i_kekerasan.php
+++ application/views/informasi/i_kekerasan.php
@@ -7,15 +7,84 @@
             <div class="box-body">
               <div class="box-group" id="accordion">
                 <!-- we are adding the .panel class so bootstrap.js collapse plugin detects it -->
+
+                 <div class="panel box box-warning">
+                  <div class="box-header with-border">
+                    <h4 class="box-title">
+                      <a data-toggle="collapse" data-parent="#accordion" href="#collapsesix" aria-expanded="true" class="">
+                        Latar Belakang
+                      </a>
+                    </h4>
+                  </div>
+                  <div id="collapsesix" class="panel-collapse collapse " aria-expanded="true" style="">
+                    <div class="box-body">
+                    <p>PEMBENTUKAN P2TP2A<br /> PERLUNYA WADAH UNTUK MEMFASILITASI PEMECAHAN MASALAH YANG DIALAMI OLEH PEREMPUAN/MASYARAKAT DI BIDANG:</p>
+<ol>
+<li>Pendidikan;</li>
+<li>Kesehatan dan Lingkungan Hidup</li>
+<li>Ekonomi dan Tenaga Kerja;</li>
+<li>Hukum, Politik dan Pengambilan Keputusan</li>
+<li>Informasi,</li>
+<li>Perlindungan Tindak Kekerasan dan Perdagangan Orang</li>
+</ol>
+<p style="text-align: center;"><strong>Visi dan Misi</strong> : </p>
+<p><strong>Visi</strong> :</p>
+<p>Terwujudnya anak dan perempuan di Provinsi Lampung yang terhindar dari ancaman kejahatan dan tindak pidana lainnya demi menegakkan Hak Azasi Manusia sesuai dengan harkat dan martabatnya sebagai Warga Negara</p>
+<p><strong>Misi</strong> :</p>
+<p>Memberikan layanan secara mudah dan cepat kepada korban</p>
+<ul>
+<li>Menyelenggarakan perlindungan dan pemenuhan rehabilitasi kesehatan, pemulangan, reintegrasi sosial dan bantuan hukum</li>
+<li>Melakukan jejaring dengan Rumah Sakit dan Dinas Sosial untuk penagnanan korban melalui rujukan</li>
+<li>Melakukankerjasama lembaga pemerintah antar Provinsi dalam rangka Rehabilitasi Sosial pemulangan korban</li>
+</ul>
+<p style="text-align: center;"><br /> <strong>Peran P2TP2A</strong></p>
+<ul>
+<li>Wahana pelayanan pemberdayaan perempuan dan perlindungan anak yang berbasis masyarakat</li>
+<li>pelayanan yang berperan dalam upaya pemenuhan kebutuhan peningkatan pendidikan, kesehatan, ekonomi, penanggulangan tindak kekerasan terhadap perempuan dan perlindungan anak serta peningkatan posisi dan kondisi perempuan dalam masyarakat</li>
+<li>Upaya untuk mewujudkan kesetaraan dan keadilan gender antara laki-laki dan perempuan, mengingat kualitas hidup perempuan masih jauh tertinggal dari laki-laki hampir di seluruh bidang pembangunan terutama bidang pendidikan, kesehatan dan ekonomi.</li>
+<li>Pusat data dan informasi, pusat rujukan dan memberikan berbagai jenis pelayanan baik fisik maupun psikologis.</li>
+</ul>
+<p style="text-align: center;"><strong>Dasar Pembentukan</strong> <br /> <br /> Instruksi Presiden No. 9 tahun 2000 tentang Pengarusutamaan Gender dalam Pembangunan Nasional;</p>
+<ol>
+<li>Konferensi Dunia tentang Perempuan ke-5 Tahun 2000 di New York;</li>
+<li>UU No. 23 tahun 2002 tentang Perlindungan Anak;</li>
+<li>UU No. 12 Tahun 2003 tentang Pemilu yang mengamanatkan kepada Partai Politik peserta Pemilu untuk dapat mencalonkan anggota DPR-RI, DPRD Propinsi dan DPRD Kabupaten/ Kota dengan memperhatikan Keterwakilan Perempuan sekurang-kurangnya 30%;</li>
+</ol>
+<p>UU No. 23 tahun 2004 tentang Penghapusan Kekerasan dalam Rumah Tangga;</p>
+<p style="text-align: center;"><strong>Pendampingan Kasus:</strong></p>
+<p><strong><em>Secara Hukum</em></strong><em>:</em></p>
+<ol>
+<li>Di Kepolisian pendampingan pada saat pelaporan, pembuatan Berita Acara Pemeriksaan (BAP), memberikan masukan pasal-pasal yang digunakan, lobby dan monitoring perkembangan kasus.</li>
+<li>Di Kejaksaan melakukan lobby dalam penyusunan dakwaan, tuntutan dan monitoring perkembangan kasus.</li>
+<li>Di Pengadilan pendampingan pada saat persidangan, membuat surat dukungan untuk tuntutan dan putusan, Monitoring perkembangan kasus, Mengusulkan JPU agar menerima atau banding terhadap hasil putusan.</li>
+</ol>
+<p>&nbsp;</p>
+<p><strong><em>Secara Non Hukum</em></strong>:</p>
+<ol>
+<li>Bantuan dan pendampingan medis untuk pengobatan luka dan <em>visum et repertum</em></li>
+<li>Bantuan psikososial dan spiritual dengan konseling rutin untuk mengatasi dampak trauma&agrave; kembali percaya diri</li>
+<li>Rujukan ke <em>shelter/</em>rumah aman&agrave; kerjasama dengan Dinas Sosial Lampung</li>
+<li>Pelayanan sosial dan akses keterampilan&agrave; pendidikan life skill</li>
+<li>Menguatkan korban untuk menyelesaikan kasusnya melalui jalur hukum dan memberikan pendampingan</li>
+<li>Pemberian bantuan modal dalam rangka penguatan ekonomi korban pasca kasus, kerjasama dengan&nbsp; Dinsos Provinsi Lampung</li>
+</ol>
+                    </div>
+                  </div>
+                </div>
+
+
+
+
+
                 <div class="panel box box-primary">
                   <div class="box-header with-border">
                     <h4 class="box-title">
-                      <a data-toggle="collapse" data-parent="#accordion" href="#collapseOne" aria-expanded="true" class="">
+                      <a data-toggle="collapse" data-parent="#accordion" href="#collapseOne" aria-expanded="false" class="">
                         Informasi kekerasan
                       </a>
                     </h4>
                   </div>
-                  <div id="collapseOne" class="panel-collapse collapse in" aria-expanded="true" style="">
+                  <div id="collapseOne" class="panel-collapse collapse " aria-expanded="false" style="">
                     <div class="box-body">
                       <p><strong>Informasi Jenis Kekerasan Pada Perempuan dan Anak :</strong></p>
 <ol>
@@ -85,12 +154,12 @@ Informasi tentang P2TP2A Provinsi Lampung                      </a>
                 <div class="panel box box-primary">
                   <div class="box-header with-border">
                     <h4 class="box-title">
-                      <a data-toggle="collapse" data-parent="#accordion1" href="#collapse3" aria-expanded="true" class="">
+                      <a data-toggle="collapse" data-parent="#accordion1" href="#collapse3" aria-expanded="false" class="">
                         Informasi tentang Kemitraan P2TP2A Provinsi Lampung
                       </a>
                     </h4>
                   </div>
-                  <div id="collapse3" class="panel-collapse collapse in" aria-expanded="true" style="">
+                  <div id="collapse3" class="panel-collapse collapse " aria-expanded="false" style="">
                     <div class="box-body">
 
                      <center><img src="./img/Picture1.png" class="img-thumbnail img-responsive"
